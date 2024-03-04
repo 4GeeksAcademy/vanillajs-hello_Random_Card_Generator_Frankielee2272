b2e13a671cf1c8f5a84843dd1fc24e3c08685a4c
Frank Trujillo <155175699+Frankielee2272@users.noreply.github.com>
2024-03-04 17:46:05 +0000

    First Commit

--- src/app.js
+++ src/app.js
@@ -8,4 +8,19 @@ import "./assets/img/4geeks.ico";
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
+
+  var cards = document.getElementsByClassName("card");
+  for (var i = 0; i < cards.length; i++) {
+    cards[i].style.display = "none";
+  }
+  var randomIndex = Math.floor(Math.random() * cards.length);
+  cards[randomIndex].style.display = "block";
+
+  document.getElementById("flipCard").addEventListener("click", function() {
+    for (var i = 0; i < cards.length; i++) {
+      cards[i].style.display = "none";
+    }
+    randomIndex = Math.floor(Math.random() * cards.length);
+    cards[randomIndex].style.display = "block";
+  });
 };
